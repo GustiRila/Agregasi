1d46b728116c08da451a1a9df5e1a8968ae3a3d9
GustiRila <praja.rila31@gmail.com>
2019-07-02 14:49:30 +0700

    Agregasi

---
+++ LatihanAgregasi/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanAgregasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan();
+            karyawan1.Nama = "Paijo";
+
+            Karyawan karyawan2 = new Karyawan();
+            karyawan2.Nama = "Jhono";
+
+            Perusahaan perusahaan = new Perusahaan();
+
+            perusahaan.DaftarKaryawan.Add(karyawan1);
+            perusahaan.DaftarKaryawan.Add(karyawan2);
+
+            int noUrut = 1;
+
+            Console.WriteLine("Daftar karyawan: ");
+            foreach (Karyawan karyawan in perusahaan.DaftarKaryawan)
+            {
+                Console.WriteLine("{0}. {1}", noUrut, karyawan.Nama);
+                noUrut++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
